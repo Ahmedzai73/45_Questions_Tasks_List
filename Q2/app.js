fc0f4b083532d954efba19340a319757425abdab
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-08 21:39:30 +0500

    #45_Questions_Tasks_List - Question no: 2
    Personal Message: Store a person’s name in a variable, and print a message to that person.
    Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”

---
+++ Q2/app.js
@@ -0,0 +1,14 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 2
+
+Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+*/
+var personName = "Eric";
+console.log("hello ".concat(personName, " would you like to learn some Python today?"));
+var personName1 = "Ahmed";
+console.log("hello ".concat(personName1, " Did you complete your Second Question Task?"));
+var personName2 = "Sir Muhammad Osman";
+console.log("Yes! ".concat(personName2, " i completed"));
---
+++ Q2/app.ts
@@ -0,0 +1,18 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 2
+
+Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+*/
+
+let personName:String = "Eric";
+console.log(`hello ${personName} would you like to learn some Python today?`);
+
+
+let personName1:String = "Ahmed";
+console.log(`hello ${personName1} Did you complete your Second Question Task?`);
+
+let personName2:String = "Sir Muhammad Osman";
+console.log(`Yes! ${personName2} i completed`);
