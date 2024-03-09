df93ba7cf8051a851c84e53671b19acd2f049be0
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-09 12:18:20 +0500

    Add files via upload

---
+++ Q3/app.ts
@@ -0,0 +1,19 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 3
+
+Name Cases: Store a person’s name in a variable, and then print that person’s name in 
+lowercase, uppercase, and titlecase.
+
+*/
+
+let personName:string = "Ahmed";
+
+// this is lowercase fonts
+console.log("Lowercase:", personName.toLowerCase());
+
+// THIS IS UPPERCASE FONTS
+console.log("UPPERCASE:", personName.toUpperCase());
+
+// This is Titlecase Fonts
+console.log("Titlecase:", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
