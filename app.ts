e8c49825510334d5781883234df4204f8116a624
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-09 15:37:45 +0500

    #45_Questions_Tasks_List - Question no: 4
    Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
    Your output should look something like the following, including the quotation marks:

---
+++ app.ts
@@ -0,0 +1,13 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 4
+
+Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+
+*/
+
+let famousPerson: string = "Lady Gaga";
+let quote: string =`"I'm not here to be perfect, I'm here to be real." - quote by: ${famousPerson}`;
+
+console.log(quote);
