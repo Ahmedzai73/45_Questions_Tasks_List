76cd4b25f79eb0b7fbd50a7492df1da1f155ca32
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-14 17:49:12 +0500

    #45_Questions_Tasks_List - Question no: 10
    Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
    If you don’t have anything specific to write because your programs are too simple at this point, 
    just add your name and the current date at the top of each program file. Then write one sentence 
    describing what the program does.

---
+++ Q10/app.ts
@@ -0,0 +1,17 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 10
+
+Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+If you don’t have anything specific to write because your programs are too simple at this point, 
+just add your name and the current date at the top of each program file. Then write one sentence 
+describing what the program does.
+
+*/
+
+
+// Ahmed | Date: 14/03/2024
+// In this variable we are adding some personal message to other people's
+
+let personalMessage:string = "Success is not the key to happiness. Happiness is the key to success If you love what you are doing, you will be successful.";
+console.log(personalMessage);
