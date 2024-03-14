08c26210e51598dc547047521c607ed7e146d545
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-14 15:34:27 +0500

    #45_Questions_Tasks_List - Question no: 7
    Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations 
    in print statements to see the results.

---
+++ Q7/app.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+
+#45_Questions_Tasks_List - Question no: 7
+
+Number Eight: Write addition, subtraction, multiplication, and division
+operations that each result in the number 8. Be sure to enclose your operations
+in print statements to see the results.
+
+*/
+// in this variable we add values for addition
+let addition = 3 + 7;
+// in this variable we add values for subtraction
+let subtraction = 20 - 10;
+// in this variable we add values for multiplication
+let multiplication = 10 * 3;
+// in this variable we add values for division
+let division = 50 / 5;
+console.log(`addition: ${addition} | subtraction: ${subtraction} | multiplication: ${multiplication} | division: ${division}`);
