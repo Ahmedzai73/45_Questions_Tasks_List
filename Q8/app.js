f2800efaa63a9086c3a6a01bcc79e7c2271692c9
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-14 15:54:16 +0500

    #45_Questions_Tasks_List - Question no: 8
    You should create four lines that look like this:
    console.log(5 + 3)

---
+++ Q8/app.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+
+#45_Questions_Tasks_List - Question no: 8
+
+You should create four lines that look like this:
+console.log(5 + 3)
+
+*/
+console.log(10 + 5); // addition
+console.log(10 - 5); // subtraction
+console.log(10 * 5); // multiplication
+console.log(10 / 5); // division
---
+++ Q8/app.ts
@@ -0,0 +1,16 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 8
+
+You should create four lines that look like this:
+console.log(5 + 3)
+
+*/
+
+console.log(10 + 5); // addition
+
+console.log(10 - 5); // subtraction
+
+console.log(10 * 5); // multiplication
+
+console.log(10 / 5); // division
