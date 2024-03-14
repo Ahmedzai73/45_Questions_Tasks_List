96409b6d8147ed443edb4c9b7001e83148c61593
Ahmedzai73 <162092321+Ahmedzai73@users.noreply.github.com>
2024-03-14 17:25:05 +0500

    #45_Questions_Tasks_List - Question no: 9
    Favorite Number: Store your favorite number in a variable. Then, using that variable, 
    create a message that reveals your favorite number. Print that message

---
+++ Q9/app.ts
@@ -0,0 +1,11 @@
+/*
+
+#45_Questions_Tasks_List - Question no: 9
+
+Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+create a message that reveals your favorite number. Print that message
+
+*/
+
+let luckyNumber: number = 18;
+console.log (`This is my lucky number ${luckyNumber} and i dont belive in lucky number :P`);
